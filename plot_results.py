fb1124b1c05174262b19d725a11ff164f154b6ee
Alekxos <mckeehan@stanford.edu>
2023-07-12 22:01:18 -0700

    Add functionality to plot n best trajectories (measured via final total loss)

--- plot_results.py
+++ plot_results.py
@@ -1,7 +1,9 @@
 # Optionally plot circuit spectrum if desired
 # spectrum_optimal_circuit(loss_record, codename="Fluxonium")
 
-from functions import get_element_counts, code_to_codename
+from collections import OrderedDict
+
+from functions import code_to_codename, flatten, get_element_counts
 from loss import OMEGA_TARGET
 from settings import RESULTS_DIR
 
@@ -12,14 +14,43 @@ import dill as pickle
 # Assign keyword arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--codes', type=str, required=True)
-parser.add_argument("num_runs")
+parser.add_argument("num_runs", type=int)
+parser.add_argument('-b', '--best_n', type=int)
 args = parser.parse_args()
+
+num_runs = int(args.num_runs)
+if args.best_n is not None:
+    best_n = args.best_n
+else:
+    best_n = num_runs
 
 def load_record(url):
     file = open(url, 'rb')
     record = pickle.load(file)
     file.close()
     return record
+
+def get_optimal_n_keys(loss_record, n, code=None):
+    optimal_keys_losses = OrderedDict({})
+
+    for circuit, circuit_code, l in loss_record.keys():
+        key = (circuit, circuit_code, 'total_loss')
+        loss = loss_record[key][-1]
+        if (code in key or code is None) and key not in optimal_keys_losses:
+
+            if len(optimal_keys_losses) < n:
+                optimal_keys_losses[key] = loss
+            else:
+                last_key = next(reversed(optimal_keys_losses))
+                if loss < optimal_keys_losses[last_key]:
+                    del optimal_keys_losses[last_key]
+                    optimal_keys_losses[key] = loss
+            optimal_keys_losses = OrderedDict(sorted(optimal_keys_losses.items(),
+                                                     key = lambda t: t[1]))
+
+    print(f"optimal_keys_losses.keys(): {list(optimal_keys_losses.keys())}")
+    return list(optimal_keys_losses.keys())
+
 
 def get_optimal_key(loss_record, code=None):
   optimal_loss = 1e100
@@ -65,7 +96,7 @@ def plot_results(loss_record, circuit_codes, type='metrics'):
             alpha = None
             linestyle = None
         else:
-            alpha = 0.1
+            alpha = 0.3
             linestyle = '--'
 
         for plot_idx in range(6):
@@ -73,9 +104,9 @@ def plot_results(loss_record, circuit_codes, type='metrics'):
                            (circuit, code, record_keys[plot_idx])],
                            plot_scheme[codename], label=label, alpha=alpha,
                            linestyle=linestyle)
-            if record_keys[plot_idx] == 'flux_sensitivity_loss':
-                print(loss_record[
-                           (circuit, code, record_keys[plot_idx])])
+            # if record_keys[plot_idx] == 'flux_sensitivity_loss':
+            #     print(loss_record[
+            #                (circuit, code, record_keys[plot_idx])])
         axs[0, 0].legend(loc='upper right')
         axs[1, 0].legend(loc='lower right')
         axs[0, 1].legend(loc='upper right')
@@ -89,10 +120,12 @@ def plot_results(loss_record, circuit_codes, type='metrics'):
 
     optimal_keys = [get_optimal_key(loss_record, code=code) for code
                     in circuit_codes]
+    optimal_n_keys = flatten([get_optimal_n_keys(loss_record, n=best_n, code=code) for code
+                    in circuit_codes])
 
     plotted_codenames = set()
-    for key in loss_record.keys():
-        circuit, code, index = key
+    for key in optimal_n_keys:
+        circuit, code, _ = key
         show_label = False
         junction_count, inductor_count, _ = get_element_counts(circuit)
         # codename = lookup_codename(junction_count, inductor_count)
@@ -107,11 +140,10 @@ def plot_results(loss_record, circuit_codes, type='metrics'):
 
 def main():
     circuit_codes = [code for code in args.codes.split(',')]
-    num_runs = int(args.num_runs)
     aggregate_loss_record = {}
     aggregate_metrics_record = {}
     for codename in circuit_codes:
-        for id in range(num_runs):
+        for id in range(1, num_runs + 1):
             loss_record = load_record(f'{RESULTS_DIR}/loss_record_{codename}_{id}.pickle')
             metrics_record = load_record(
                 f'{RESULTS_DIR}/metrics_record_{codename}_{id}.pickle')
