2a18ab0a22e1f04e2280b8d83dfb9154402106f9
sambonkov <sblaschke3@gmail.com>
2023-08-29 21:41:40 -0700

    Update flux, charge sensitivity to not alter old eigenvectors

--- qubit_discovery/losses/functions.py
+++ qubit_discovery/losses/functions.py
@@ -1,5 +1,6 @@
 """Contains helper functions used in remainder of code."""
 
+from copy import copy
 from typing import Union
 
 import numpy as np
@@ -13,6 +14,8 @@ SQArrType = Union[np.ndarray, torch.Tensor]
 SQValType = Union[float, torch.Tensor]
 
 # Helper functions
+# NOTE: Ensure all functions treat the input `circuit` as const, at least
+# in effect.
 
 def first_resonant_frequency(circuit: Circuit) -> SQValType:
     """Calculates resonant frequency of first excited eigenstate in circuit."""
@@ -34,32 +37,34 @@ def charge_sensitivity(circuit: Circuit,
     (as opposed to e.g. flux sensitivity, which considers perturbations around
     flux operation point)."""
 
-    return torch.as_tensor(epsilon) # temporary
-
     # Edge case: For circuit with no charge modes, assign zero sensitivity
-    if sum(circuit.omega == 0) == 0:
+    if np.all(circuit.omega != 0):
         if get_optim_mode():
             return torch.as_tensor(epsilon)
         else:
             return epsilon
 
     c_0 = circuit.efreqs[1] - circuit.efreqs[0]
-    # new_circuit = copy(circuit)
+
+    # Copy circuit to create new container for perturbed eigenstates
+    perturb_circ = copy(circuit)
     # For each mode, if charge mode exists then set gate charge to obtain
     # minimum frequency
-    for charge_island_idx in test_circuit.charge_islands.keys():
+    for charge_island_idx in perturb_circ.charge_islands.keys():
         charge_mode = charge_island_idx + 1
         # Set gate charge to 0.5 in each mode
         # (to extremize relative to n_g=0)
-        test_circuit.set_charge_offset(charge_mode, 0.5)
+        perturb_circ.set_charge_offset(charge_mode, 0.5)
 
-    test_circuit.diag(len(circuit.efreqs))
-    c_delta = test_circuit.efreqs[1] - test_circuit.efreqs[0]
+    perturb_circ.diag(len(circuit.efreqs))
+    c_delta = perturb_circ.efreqs[1] - perturb_circ.efreqs[0]
 
-    for charge_island_idx in test_circuit.charge_islands.keys():
+    # Reset charge modes; this is necessary because peturb_circ and
+    # circ are basically the same
+    for charge_island_idx in circuit.charge_islands.keys():
         charge_mode = charge_island_idx + 1
         # Reset charge modes in test circuit
-        test_circuit.set_charge_offset(charge_mode, 0.)
+        perturb_circ.set_charge_offset(charge_mode, 0.)
 
     if get_optim_mode():
         return torch.abs((c_delta - c_0) / ((c_delta + c_0) / 2))
@@ -75,13 +80,17 @@ def flux_sensitivity(
     """Return the flux sensitivity of the circuit around half flux quantum."""
     f_0 = circuit.efreqs[1] - circuit.efreqs[0]
 
-    loop = circuit.loops[0]
-    org_flux = loop.value() # should be `flux_point`
+    # Copy circuit to create new container for perturbed eigenstates
+    perturb_circ = copy(circuit)
+    loop = perturb_circ.loops[0]
+    org_flux = loop.value() / (2 * np.pi) # should be `flux_point`
 
+    # Change the flux and get the eigenfrequencies
     loop.set_flux(flux_point + delta)
-    circuit.diag(len(circuit.efreqs))
-    f_delta = circuit.efreqs[1] - circuit.efreqs[0]
+    perturb_circ.diag(len(circuit.efreqs))
+    f_delta = perturb_circ.efreqs[1] - perturb_circ.efreqs[0]
 
+    # Return loop back to original flux
     loop.set_flux(org_flux)
 
     if get_optim_mode():
