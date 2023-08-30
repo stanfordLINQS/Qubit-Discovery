3d37a4246700e3508f26b0355411f959ddf24d55
sambonkov <sblaschke3@gmail.com>
2023-08-29 21:43:33 -0700

    Cleanup truncation

--- qubit_discovery/optimization/truncation.py
+++ qubit_discovery/optimization/truncation.py
@@ -5,28 +5,33 @@ from typing import Tuple, List, Optional
 import numpy as np
 from matplotlib.axes import Axes
 import scipy, scipy.signal
-from SQcircuit import Circuit
+from SQcircuit import get_optim_mode, Circuit
 
 def get_reshaped_eigvec(
         circuit: Circuit,
         eig_vec_idx: int,
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Return the eigenvec, index1_eigenvec and index2_eigenvec part of
-  the eigenvectors."""
+) -> Tuple[np.ndarray, Tuple[np.ndarray, ...]]:
+    """
+    Return the eigenvec, index1_eigenvec and index2_eigenvec part of
+    the eigenvectors.
+    """
 
-    assert len(circuit._efreqs) != 0, "circuit should be diagonalizecd first."
+    assert len(circuit._efreqs) != 0, "circuit should be diagonalized first."
 
     # Reshape eigenvector dimensions to correspond to individual modes
-    eigenvector = np.array(circuit._evecs[eig_vec_idx].detach().numpy())
+    if get_optim_mode():
+        eigenvector = np.array(circuit._evecs[eig_vec_idx].detach().numpy())
+    else:
+        eigenvector = circuit._evecs[eig_vec_idx]
     eigenvector_reshaped = np.reshape(eigenvector, circuit.m)
 
     # Extract maximum magnitudes of eigenvector entries along each mode axis
-    mode_2_magnitudes = np.amax(np.abs(eigenvector_reshaped) ** 2, axis=1)
+    mode_2_magnitudes = np.max(np.abs(eigenvector_reshaped) ** 2, axis=1)
     offset_idx = np.argmax(mode_2_magnitudes)
     mode_1_magnitudes = np.abs(eigenvector_reshaped[offset_idx, :]) ** 2
     eigvec_mag = np.abs(eigenvector) ** 2
 
-    return eigvec_mag, mode_1_magnitudes, mode_2_magnitudes
+    return eigvec_mag, (mode_1_magnitudes, mode_2_magnitudes)
 
 
 def fit_mode(
@@ -37,7 +42,8 @@ def fit_mode(
         axis: Optional[Axes]=None,
         both_parities=False
 ) -> List[Tuple[float, float, int]]:
-    '''For an input vector corresponding to the absolute eigenvector magnitudes
+    """
+    For an input vector corresponding to the absolute eigenvector magnitudes
     for a specific harmonic mode, return the decay constant for an exponential
     decay fit starting with the last peak of the input.
 
@@ -47,7 +53,7 @@ def fit_mode(
 
     If both_parities is false, returns the fit corresponding to those indices
     with the same parity as the maximum element in the list.
-    '''
+    """
 
     def monoExp(x, m, k):
         return m * np.exp(-k * x)
@@ -120,18 +126,20 @@ def trunc_num_heuristic(
         circuit: Circuit,
         eig_vec_idx: int = 0,
         K: int=1000,
-        seed: int=11,
         min_trunc: int=4,
         axes: Optional[Axes]=None
-) -> float:
-    '''For a diagonalized circuit with internal trunc numbers, suggests a set of
+) -> List[int]:
+    """
+    For a diagonalized circuit with internal trunc numbers, suggests a set of
     trunc numbers for rediagonalization that will maximize likelihood of
     convergence, defined under the `convergence_test` function.
-    '''
+    """
     assert len(circuit._efreqs) != 0, "Circuit should be diagonalized first"
 
     trunc_nums = circuit.m
 
+    # Ensure each mode has at least `min_trunc` by allocating truncation
+    # numbers as proportion of `min_trunc`
     K = int(K / min_trunc ** 2)
 
     _, mode_1_magnitudes, mode_2_magnitudes = get_reshaped_eigvec(
@@ -177,51 +185,70 @@ def trunc_num_heuristic(
     if mode_2_result == 0:
         mode_2_result = 1
 
+    # When returning, go back from proportion of `sqrt(min_trunc)`
     return [mode_1_result * min_trunc, mode_2_result * min_trunc]
-
-def test_convergence(
-    circuit: Circuit,
-    eig_vec_idx: int = 0,
-    t: int = 10,
-    threshold: float = 1e-4,
-) -> Tuple[bool, float, float]:
-
-    assert len(circuit._efreqs) != 0, "Circuit should be diagonalized first"
-
-    if len(circuit.m) == 1:
-        return circuit.test_convergence([circuit.m[0], ], eig_vec_idx=eig_vec_idx)
-
-    # Below treats case of two modes (test for more modes not yet implemented)
-
-    eigvec_mag, mode_1_magnitudes, mode_2_magnitudes = get_reshaped_eigvec(
-        circuit,
-        eig_vec_idx,
-    )
-
-    y1, y2 = mode_1_magnitudes[-t:], mode_2_magnitudes[-t:]
-    assert len(y1) >= 4 and len(y2) >= 4, "Need at least 4 modes to check both parities"
-    if len(y1) <= t:
-      epsilon_1 = (y1[-1] + y1[-2]) * (t / 2)
-    else:
-      epsilon_1 = np.sum(y1)
-    if len(y2) <= t:
-      epsilon_2 = (y2[-1] + y2[-2]) * (t / 2)
-    else:
-      epsilon_2 = np.sum(y2)
-    if epsilon_1 > threshold or epsilon_2 > threshold:
-      return False, epsilon_1, epsilon_2
-
-    return True, (epsilon_1, epsilon_2)
 
 def assign_trunc_nums(circuit: Circuit, 
                       total_trunc_num: int) -> None:
+    """
+    Heuristically re-assign truncation numbers for a circuit with one
+    or two modes (not yet implemented for more)
+    """
     if len(circuit.m) == 1:
-        print("test_convergence (one mode)")
+        print("re-allocate truncation numbers (one mode)")
         circuit.set_trunc_nums([total_trunc_num, ])
     elif len(circuit.m) == 2:
-        print("test_convergence (two modes)")
+        print("re-allocate truncation numbers (two modes)")
         trunc_nums = trunc_num_heuristic(circuit,
                                          K=total_trunc_num,
                                          eig_vec_idx=1,
                                          axes=None)
         circuit.set_trunc_nums(trunc_nums)
+    else:
+        raise NotImplementedError
+
+def test_convergence(
+    circuit: Circuit,
+    eig_vec_idx: int = 0,
+    t: int = 10,
+    threshold: float = 1e-5,
+) -> Tuple[bool, Tuple[float, ...]]:
+    """
+    Test convergence of a circuit with one or two modes (test for more modes
+    not yet implemented).
+
+    Requires the last `t` (if available) elements corresponding to 
+    each mode individually of the `eig_vec_idx`th eigenvector are each on 
+    average less than `threshold`.
+
+    Returns a boolean of whether the convergence test passed, and a tuple
+    of the average values of the last `t` components for each mode.
+    """
+    assert len(circuit._efreqs) != 0, "Circuit should be diagonalized first"
+
+    if len(circuit.m) == 1:
+        return circuit.test_convergence([circuit.m[0], ], eig_vec_idx=eig_vec_idx)
+    elif len(circuit.m) == 2:
+        eigvec_mag, mode_1_magnitudes, mode_2_magnitudes = get_reshaped_eigvec(
+                circuit,
+                eig_vec_idx,
+        )
+
+        y1, y2 = mode_1_magnitudes[-t:], mode_2_magnitudes[-t:]
+        assert len(y1) >= 4 and len(y2) >= 4, "Need at least 4 modes to check both parities"
+        if len(y1) <= t:
+            epsilon_1 = (y1[-1] + y1[-2]) / 2
+        else:
+            epsilon_1 = np.average(y1)
+        if len(y2) <= t:
+            epsilon_2 = (y2[-1] + y2[-2]) /2
+        else:
+            epsilon_2 = np.average(y2)
+
+        if epsilon_1 > threshold or epsilon_2 > threshold:
+            return False, (epsilon_1, epsilon_2)
+
+        return True, (epsilon_1, epsilon_2)
+    else:
+        raise NotImplementedError
+    
