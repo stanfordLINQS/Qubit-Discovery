8455dc75c4538024e97482273473a51da26df188
sambonkov <sblaschke3@gmail.com>
2023-09-15 00:37:42 -0600

    Bugfixes

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import os
 from typing import Any, Dict, List
 
@@ -18,19 +19,18 @@ def load_record(url: str) -> Any:
     except FileNotFoundError:
         return None
     
-def compute_best_ids(aggregate_loss_records, n: int, codes=List[str]
+def compute_best_ids(aggregate_loss_records, n: int, codes: List[str]
                      ) -> Dict[str, List[int]]:
     out = {}
-    for code in codes:
-        code_loss_record = [(id_num, run) for id_num, run in aggregate_loss_records 
-                            if run['circuit_code'] == code]
+    for codename in codes:
+        code_loss_record = aggregate_loss_records[codename].items()
         sorted_runs = sorted(code_loss_record, key=lambda x: x[1]['total_loss'][-1])
-        out[code] = [id_num for id_num, run in sorted_runs]
-        print(f'Top {n} runs in order are {out[code]} for code {code}.')
+        out[codename] = [id_num for id_num, run in sorted_runs]
+        print(f'Top {n} runs in order are {out[codename]} for code {codename}.')
     return out
 
 
-def plot_results(loss_record, 
+def plot_results(record, 
                  best_ids: Dict[str, List[int]],
                  plot_type: str, 
                  title: str = '',
@@ -44,7 +44,7 @@ def plot_results(loss_record,
     LOSS_TITLES = ['Frequency Loss', 'Anharmonicity Loss', 'T1 Loss',
                      'Flux Sensitivity Loss', 'Charge Sensitivity Loss', 'Total Loss']
     LOSS_KEYS = ['frequency_loss', 'anharmonicity_loss', 'T1_loss',
-                   'flux_sensitivity_loss', 'charge_sensitivity_loss', 'total_loss']
+                 'flux_sensitivity_loss', 'charge_sensitivity_loss', 'total_loss']
     
     fig, axs = plt.subplots(2, 3, figsize=(22, 11))
     fig.suptitle(title, fontsize = 32)
@@ -69,9 +69,13 @@ def plot_results(loss_record,
             linestyle = '--'
 
         for plot_idx in range(6):
-            axs[plot_idx % 2, plot_idx // 2].plot(run[record_keys[plot_idx]],
-                           PLOT_SCHEME[codename], label=label, alpha=alpha,
-                           linestyle=linestyle)
+            try:
+                axs[plot_idx % 2, plot_idx // 2].plot(run[record_keys[plot_idx]],
+                            PLOT_SCHEME[codename], label=label, alpha=alpha,
+                            linestyle=linestyle)
+            except KeyError:
+                print(plot_type)
+                print(record)
         axs[0, 0].legend(loc='upper right')
         axs[1, 0].legend(loc='lower right')
         axs[0, 1].legend(loc='upper right')
@@ -83,12 +87,13 @@ def plot_results(loss_record,
         axs[1, 0].axhline(y=OMEGA_TARGET, color='m', linestyle=':')
         axs[0, 2].axhline(y=22, color='m', linestyle=':')
 
-    for code, runs_list in best_ids:
-        # Plot best run for that specific cocde
-        plot_circuit_metrics(runs_list[0], code, True, True)
+    for codename, ids_list in best_ids.items():
+        # Plot best run for that specific code
+        runs_list = [record[codename][id_num] for id_num in ids_list]
+        plot_circuit_metrics(runs_list[0], codename, True, True)
         # Plot the remaining ones
         for run in runs_list[1:]:
-            plot_circuit_metrics(run, code, False, False)
+            plot_circuit_metrics(run, codename, False, False)
 
     plt.savefig(f'{RESULTS_DIR}/{save_prefix}_{plot_type}_record.png', dpi=300)
 
@@ -126,8 +131,8 @@ def main() -> None:
 
     name = args.name
     circuit_codes = args.codes.split(',')
-    aggregate_loss_record = []
-    aggregate_metrics_record = []
+    aggregate_loss_record = defaultdict(dict)
+    aggregate_metrics_record = defaultdict(dict)
     optim_type = args.optimization_type
 
     for codename in circuit_codes:
@@ -140,8 +145,8 @@ def main() -> None:
                 RESULTS_DIR, f'{optim_type}_metrics_record_{codename}_{identifier}.pickle'))
             
             if loss_record is not None and metrics_record is not None:
-                aggregate_loss_record.append((id_num, loss_record))
-                aggregate_metrics_record.append((id_num, metrics_record))
+                aggregate_loss_record[codename][id_num] = loss_record
+                aggregate_metrics_record[codename][id_num] = metrics_record
 
     save_prefix = build_save_prefix(args)
     title = f"Optimization with {args.optimization_type}"
@@ -149,7 +154,7 @@ def main() -> None:
         title += f': {name}.'
 
 
-    best_ids = compute_best_ids(aggregate_loss_record, best_n)
+    best_ids = compute_best_ids(aggregate_loss_record, best_n, circuit_codes)
     plot_results(aggregate_loss_record, best_ids, plot_type='loss', title=title,
                  save_prefix=save_prefix)
     plot_results(aggregate_metrics_record, best_ids, plot_type='metrics', title=title,
