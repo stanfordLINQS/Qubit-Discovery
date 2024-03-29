77882c9013251a1a63e02474843815a710a285d0
taha1373 <tahar@stanford.edu>
2024-03-28 19:18:30 -0700

    adding loss_record test for SGD

--- qubit_discovery/tests/test_SGD.py
+++ qubit_discovery/tests/test_SGD.py
@@ -1,6 +1,7 @@
 """Test Module for BFGS algorithm."""
 
 import torch
+import numpy as np
 
 import SQcircuit as sq
 
@@ -11,6 +12,7 @@ from qubit_discovery.losses import loss_functions
 from qubit_discovery.tests.conftest import (
     get_fluxonium,
     get_bounds,
+    are_loss_dicts_close,
 )
 
 
@@ -21,6 +23,15 @@ def test_sgd_run() -> None:
         [1.9758e-14, 1.6673e-07, 6.4088e+09],
         dtype=torch.float64
     )
+
+    target_loss_record = {
+        'frequency_loss': [np.array(5.48357335)],
+        'anharmonicity_loss': [np.array(1.42174492)],
+        'T1_loss': [np.array(42581151.89052253)],
+        'flux_sensitivity_loss': [np.array(1.e-14)],
+        'charge_sensitivity_loss': [np.array(0.)],
+        'total_loss': [np.array(42581158.79584081)],
+    }
 
     def my_loss_function(cr: Circuit):
         return loss_functions['default'](
@@ -38,7 +49,7 @@ def test_sgd_run() -> None:
     circuit = get_fluxonium()
     circuit.set_trunc_nums([total_trunc_num])
 
-    run_SGD(
+    loss_record = run_SGD(
         circuit=circuit,
         circuit_code="JL",
         loss_metric_function=my_loss_function,
@@ -51,4 +62,7 @@ def test_sgd_run() -> None:
         save_intermediate_circuits=False,
     )
 
+    del loss_record['circuit_code']
+
     assert torch.isclose(torch.stack(circuit.parameters), target_params).all()
+    assert are_loss_dicts_close(loss_record, target_loss_record, rel=1e-2)
