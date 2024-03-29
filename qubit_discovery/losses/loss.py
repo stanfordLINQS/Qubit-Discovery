f090b9a47fb001368abc753848920323a01f0533
taha1373 <tahar@stanford.edu>
2024-03-28 19:13:57 -0700

    adding a new loss function for easier loss construction

--- qubit_discovery/losses/loss.py
+++ qubit_discovery/losses/loss.py
@@ -1,13 +1,12 @@
 """Contains code for defining loss functions used in circuit optimization."""
 from collections import defaultdict
 from copy import deepcopy
-from typing import TypedDict, Tuple
+from typing import List, TypedDict, Tuple, Dict
 
 from .functions import (
     calculate_anharmonicity,
     charge_sensitivity,
     flux_sensitivity,
-    flux_sensitivity_constantnorm,
     first_resonant_frequency,
     reset_charge_modes,
     SQValType,
@@ -52,36 +51,14 @@ def anharmonicity_loss(
     return loss, anharmonicity
 
 
-def anharmonicity_loss_constantnorm(
-    circuit: Circuit,
-    alpha=1,
-    epsilon=1e-14
-) -> Tuple[SQValType, SQValType]:
-    """Designed to penalize energy level occupancy in the vicinity of
-    ground state or twice resonant frequency"""
-    message = "Anharmonicity is only defined for at least three energy levels."
-    assert len(circuit.efreqs) > 2, message
-    omega_10 = circuit.efreqs[1] - circuit.efreqs[0]
-    omega_i0 = circuit.efreqs[2:] - circuit.efreqs[0]
-    x1 = alpha * (omega_i0 - 2 * omega_10) / OMEGA_TARGET
-    x2 = alpha * (omega_i0 - omega_10) / OMEGA_TARGET
-    
-    anharmonicity = calculate_anharmonicity(circuit)
-    if get_optim_mode():
-        loss = torch.sum(torch.exp(-torch.abs(x1)) + torch.exp(-torch.abs(x2))) + epsilon
-    else:
-        loss = np.sum(np.exp(-np.abs(x1)) + np.exp(-np.abs(x2))) + epsilon
-    return loss, anharmonicity
-
-
 def T1_loss(circuit: Circuit) -> Tuple[SQValType, SQValType]:
-    Gamma_1 = circuit.dec_rate('capacitive', (0, 1))
-    Gamma_2 = circuit.dec_rate('inductive', (0, 1))
-    Gamma_3 = circuit.dec_rate('quasiparticle', (0, 1))
-    Gamma = Gamma_1 + Gamma_2 + Gamma_3
-    T1 = 1 / Gamma
+    gamma_1 = circuit.dec_rate('capacitive', (0, 1))
+    gamma_2 = circuit.dec_rate('inductive', (0, 1))
+    gamma_3 = circuit.dec_rate('quasiparticle', (0, 1))
+    gamma = gamma_1 + gamma_2 + gamma_3
+    T1 = 1 / gamma
 
-    loss = Gamma ** 2
+    loss = gamma ** 2
     if not get_optim_mode():
         loss = loss.item()
 
@@ -89,13 +66,13 @@ def T1_loss(circuit: Circuit) -> Tuple[SQValType, SQValType]:
 
 
 def T2_loss(circuit: Circuit) -> Tuple[SQValType, SQValType]:
-    Gamma_1 = circuit.dec_rate('charge', (0, 1))
-    Gamma_2 = circuit.dec_rate('cc', (0, 1))
-    Gamma_3 = circuit.dec_rate('flux', (0, 1))
-    Gamma = Gamma_1 + Gamma_2 + Gamma_3
-    T2 = 1 / Gamma
+    gamma_1 = circuit.dec_rate('charge', (0, 1))
+    gamma_2 = circuit.dec_rate('cc', (0, 1))
+    gamma_3 = circuit.dec_rate('flux', (0, 1))
+    gamma = gamma_1 + gamma_2 + gamma_3
+    T2 = 1 / gamma
 
-    loss = Gamma ** 2
+    loss = gamma ** 2
     if not get_optim_mode():
         loss = loss.item()
 
@@ -112,26 +89,6 @@ def flux_sensitivity_loss(
     (typically half flux quantum)."""
 
     S = flux_sensitivity(circuit)
-
-    # Apply hinge loss
-    if S < a:
-        loss = 0.0 * S + epsilon
-    else:
-        loss = b * (S - a) + epsilon
-
-    return loss, S
-
-
-def flux_sensitivity_loss_constantnorm(
-    circuit: Circuit,
-    a=0.1,
-    b=1,
-    epsilon=1e-14
-) -> Tuple[SQValType, SQValType]:
-    """Return the flux sensitivity of the circuit around flux operation point
-    (typically half flux quantum)."""
-
-    S = flux_sensitivity_constantnorm(circuit, OMEGA_TARGET)
 
     # Apply hinge loss
     if S < a:
@@ -162,14 +119,14 @@ def charge_sensitivity_loss(
     return loss, S
 
 
-def experimental_sensitivity_loss(
+def element_sensitivity_loss(
     circuit: Circuit,
-    N_SAMPLES=10,
-    P_ERROR=1,
+    n_samples=10,
+    p_error=1,
     n_eig=10
 ) -> Tuple[SQValType, SQValType]:
-    """"Returns an estimate of parameter sensitivity, as determined by variation of
-    T1 value in Gaussian probability distribution about element values"""
+    """"Returns an estimate of parameter sensitivity, as determined by variation
+    of T1 value in Gaussian probability distribution about element values"""
     def set_elem_value(elem, val):
         elem._value = val
 
@@ -177,25 +134,34 @@ def experimental_sensitivity_loss(
     for edge in circuit.elements:
         for i, el in enumerate(circuit.elements[edge]):
             if el in list(circuit._parameters):
-                elements_to_update[edge].append((i, list(circuit._parameters).index(el)))
+                elements_to_update[edge].append(
+                    (i, list(circuit._parameters).index(el))
+                )
 
-    dist = torch.distributions.MultivariateNormal(torch.stack(circuit.parameters),
-                                                  torch.diag(((P_ERROR * torch.stack(circuit.parameters)) / 100) ** 2))
-    new_params = dist.rsample((N_SAMPLES, ))  # reparameterization trick
-    vals = torch.zeros((N_SAMPLES,))
-    for i in range(N_SAMPLES):
-        elements_sampled = deepcopy(circuit.elements) # assumes all leaf tensors in .elements
+    dist = torch.distributions.MultivariateNormal(
+        torch.stack(circuit.parameters),
+        torch.diag(((p_error * torch.stack(circuit.parameters)) / 100) ** 2)
+    )
+    # re-parameterization trick
+    new_params = dist.rsample((n_samples, ))
+    vals = torch.zeros((n_samples,))
+    for i in range(n_samples):
+        # assumes all leaf tensors in .elements
+        elements_sampled = deepcopy(circuit.elements)
         for edge in elements_to_update:
             for el_idx, param_idx in elements_to_update[edge]:
-                set_elem_value(elements_sampled[edge][el_idx], new_params[i, param_idx])
+                set_elem_value(
+                    elements_sampled[edge][el_idx],
+                    new_params[i, param_idx]
+                )
 
         cr_sampled = Circuit(elements_sampled)
         cr_sampled.set_trunc_nums(circuit.trunc_nums)
         cr_sampled.diag(n_eig)
         _, vals[i] = T1_loss(cr_sampled)
-    E = torch.std(vals) / torch.mean(vals)
-    loss = E
-    return loss, E
+    sensitivity = torch.std(vals) / torch.mean(vals)
+    loss = sensitivity
+    return loss, sensitivity
 
 
 class LossOut(TypedDict):
@@ -217,7 +183,7 @@ class MetricOut(TypedDict):
 
 
 default_functions = {
-    'omega':frequency_loss,
+    'omega': frequency_loss,
     'aharm': anharmonicity_loss,
     'T1': T1_loss,
     'flux': flux_sensitivity_loss,
@@ -230,7 +196,6 @@ def detach_if_optim(value: SQValType) -> SQValType:
     """Detach the value if is in torch. Otherwise, return the value itself."""
 
     if get_optim_mode():
-
         return value.detach()
 
     return value
@@ -243,9 +208,7 @@ def calculate_loss_metrics(
     use_anharmonicity_loss=True,
     use_flux_sensitivity_loss=True,
     use_charge_sensitivity_loss=1,
-    # use_experimental_sensitivity_loss=False,
     use_T1_loss=False,
-    use_T2_loss=False,
     log_loss=False,
     master_use_grad=True
 ) -> Tuple[SQValType, LossOut, MetricOut]:
@@ -290,24 +253,25 @@ def calculate_loss_metrics(
 
     # Calculate flux sensitivity loss
     with torch.set_grad_enabled(use_flux_sensitivity_loss and master_use_grad):
-        loss_flux_sensitivity, flux_sensitivity_value = flux_sensitivity_loss(circuit)
+        loss_flux_sensitivity, flux_sensitivity_value = (
+            flux_sensitivity_loss(circuit)
+        )
         if use_flux_sensitivity_loss:
             loss = loss + loss_flux_sensitivity
 
     # Calculate charge sensitivity loss
-    charge_sensitivity_loss_bool = (True if use_charge_sensitivity_loss != 0 else False)
-    with torch.set_grad_enabled(charge_sensitivity_loss_bool and master_use_grad):
-        loss_charge_sensitivity, charge_sensitivity_value = charge_sensitivity_loss(circuit, code=use_charge_sensitivity_loss)
+    charge_sensitivity_loss_bool = (
+        True if use_charge_sensitivity_loss != 0 else False
+    )
+    with torch.set_grad_enabled(
+            charge_sensitivity_loss_bool and master_use_grad
+    ):
+        loss_charge_sensitivity, charge_sensitivity_value = (
+            charge_sensitivity_loss(circuit, code=use_charge_sensitivity_loss)
+        )
 
         if charge_sensitivity_loss_bool:
             loss = loss + loss_charge_sensitivity
-
-    # with torch.set_grad_enabled(use_experimental_sensitivity_loss and master_use_grad):
-    #     loss_experimental_sensitivity, experimental_sensitivity_value = experimental_sensitivity_loss(circuit)
-    #     if loss_normalization:
-    #         loss_experimental_sensitivity /= loss_experimental_sensitivity_init
-    #     if use_experimental_sensitivity_loss:
-    #         loss = loss + loss_experimental_sensitivity
 
     if log_loss:
         if get_optim_mode():
@@ -322,7 +286,6 @@ def calculate_loss_metrics(
             'T1_loss': detach_if_optim(loss_T1),
             'flux_sensitivity_loss': detach_if_optim(loss_flux_sensitivity),
             'charge_sensitivity_loss': detach_if_optim(loss_charge_sensitivity),
-            # 'experimental_sensitivity_loss': detach_if_optim(loss_experimental_sensitivity)
             'total_loss': detach_if_optim(loss)
         }
         metrics: MetricOut = {
@@ -333,5 +296,78 @@ def calculate_loss_metrics(
             'charge_sensitivity': detach_if_optim(charge_sensitivity_value),
             'T2': detach_if_optim(T2_time),
         }
+
+    return loss, loss_values, metrics
+
+
+def get_all_functions() -> Dict[str, callable]:
+    """Returns a dictionary of all loss function with their proper keys."""
+
+    all_functions = {
+        'frequency': frequency_loss,
+        'anharmonicity': anharmonicity_loss,
+        'flux_sensitivity': flux_sensitivity_loss,
+        'charge_sensitivity': charge_sensitivity_loss,
+        'element_sensitivity': element_sensitivity_loss,
+        'T1': T1_loss,
+        'T2': T2_loss,
+    }
+
+    return all_functions
+
+
+use_losses_default = {
+    'anharmonicity': 1.0,
+    'flux_sensitivity': 1.0,
+    'charge_sensitivity': 1.0,
+}
+
+use_metrics_default = [
+    'T1', 'T2',
+]
+
+
+def calculate_loss_metrics_new(
+    circuit: Circuit,
+    master_use_grad: bool = True,
+    use_losses: Dict[str, float] = None,
+    use_metrics: List[str] = None,
+) -> Tuple[SQValType, Dict[str, SQValType], Dict[str, SQValType]]:
+
+    if use_metrics is None:
+        use_metrics = use_metrics_default
+
+    if use_losses is None:
+        use_losses = use_losses_default
+
+    function_dict = get_all_functions()
+
+    if get_optim_mode():
+        loss = torch.zeros((), requires_grad=master_use_grad)
+    else:
+        loss = 0.0
+
+    loss_values: Dict[str, SQValType] = {}
+    metrics: Dict[str, SQValType] = {}
+
+    for key in function_dict.keys():
+
+        if key in use_losses:
+            with torch.set_grad_enabled(master_use_grad):
+                specific_loss, specific_metric = function_dict[key](circuit)
+                loss = loss + use_losses[key] * specific_loss
+
+            with torch.no_grad():
+                loss_values[key + '_loss'] = detach_if_optim(specific_loss)
+                metrics[key] = detach_if_optim(specific_metric)
+
+        elif key not in use_losses and key in use_metrics:
+            with torch.no_grad():
+                specific_loss, specific_metric = function_dict[key](circuit)
+
+                loss_values[key + '_loss'] = detach_if_optim(specific_loss)
+                metrics[key] = detach_if_optim(specific_metric)
+
+    loss_values['total_loss'] = detach_if_optim(loss)
 
     return loss, loss_values, metrics
