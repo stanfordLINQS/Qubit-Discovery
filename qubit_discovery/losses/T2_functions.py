Finish torch nodes and fix charge derivative

qubit_discovery/losses/T2_functions.py
@@ -63,7 +63,6 @@ def partial_squared_omega(
 
     return np.real(p2_omega)
 
-
 def partial_dephasing_rate(
         A,
         partial_A,
@@ -111,7 +110,7 @@ def get_B_idx(
 
 
 ###############################################################################
-# Charge Noise
+# Charge noise
 ###############################################################################
 
 def partial_H_ng(
@@ -135,8 +134,35 @@ def partial_H_ng(
             * cr._memory_ops["Q"][j]
             / np.sqrt(unt.hbar)
         )
-    return -op
+    return op
 
+def partial_squared_H_ng(
+    cr: Circuit,
+    charge_idx: int,
+    grad_el: Union[Capacitor, Inductor, Junction]
+):
+    """ Calculates the second derivative of the Hamiltonian of `cr` with 
+    respect to the gate charge on the `charge_idx` charge mode and `grad_el`
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object to differentiate the Hamiltonian of
+        charge_idx:
+            The charge mode whose gate charge to differentiate with respect to
+        grad_el:
+            The circuit element to differentiate with respect to
+    """
+    if not isinstance(grad_el, Capacitor):
+        return 0
+
+    cInv = np.linalg.inv(sqf.numpy(cr.C))
+    A = cInv @ cr.partial_mats[grad_el] @ cInv
+
+    op = qt.Qobj()
+    for j in range(cr.n):
+        op += A[charge_idx, j] * cr._memory_ops["Q"][j] / np.sqrt(unt.hbar)
+    return -op
 
 def partial_omega_ng(
     cr: Circuit,
@@ -156,47 +182,17 @@ def partial_omega_ng(
         states:
             The numbers `(m, n)` of the eigenfrequencies to differentiate.
     """
-    state1 = sqf.qutip(cr._evecs[states[0]], dims=cr._get_state_dims())
-    state2 = sqf.qutip(cr._evecs[states[1]], dims=cr._get_state_dims())
+    state_m = sqf.qutip(cr._evecs[states[0]], dims=cr._get_state_dims())
+    state_n = sqf.qutip(cr._evecs[states[1]], dims=cr._get_state_dims())
     op = partial_H_ng(cr, charge_idx)
 
-    partial_omega = (
-        state2.dag() * (op * state2) -
-        - state1.dag() * (op * state1)
+    partial_omega_mn = (
+        state_m.dag() * (op * state_m)
+        - state_n.dag() * (op * state_n)
     )[0, 0]
-    assert np.imag(partial_omega)/np.real(partial_omega) < 1e-6
+    assert np.imag(partial_omega_mn)/np.real(partial_omega_mn) < 1e-5
 
-    return np.real(partial_omega)
-
-
-def partial_squared_H_ng(
-    cr: Circuit,
-    charge_idx: int,
-    grad_el: Union[Capacitor, Inductor, Junction]
-):
-    """ Calculates the second derivative of the Hamiltonian of `cr` with 
-    respect to the gate charge on the `charge_idx` charge mode and `grad_el`
-
-    Parameters
-    ----------
-        cr:
-            The `Circuit` object to differentiate the Hamiltonian of
-        charge_idx:
-            The charge mode whose gate charge to differentiate with respect to
-        grad_el:
-            The circuit element to differentiate with respect to
-    """
-    if isinstance(grad_el, Capacitor):
-        return 0
-
-    cInv = np.linalg.inv(sqf.numpy(cr.C))
-    A = cInv @ cr.partial_mats[grad_el] @ cInv
-
-    op = qt.Qobj()
-    for j in range(cr.n):
-        op += A[charge_idx, j] * cr._memory_ops["Q"][j] / np.sqrt(unt.hbar)
-    return op
-
+    return np.real(partial_omega_mn)
 
 def partial_squared_omega_mn_ng(
     cr: Circuit,
@@ -230,7 +226,17 @@ def partial_charge_dec(
     states: Tuple[int, int]
 ):
     """
-    The A is independent of all elements
+    Calculate the derivative of the charge decoherence between the states given 
+    in `states` with respect to `grad_el`.
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object.
+        states:
+            A tuple `(m, n)` of states to consider charge decoherence between.
+        grad_el:
+            The circuit element to differentiate with respect to
     """
     dec_rate_grad = 0
     for i in range(cr.n):
@@ -253,7 +259,7 @@ def partial_charge_dec(
     return dec_rate_grad
 
 ###############################################################################
-# Critical Current Noise
+# Critical current noise
 ###############################################################################
 
 def partial_squared_omega_mn_EJ(
@@ -291,6 +297,18 @@ def partial_cc_dec(
     grad_el: Element,
     states: Tuple[int, int]
 ):
+    """ Calculate the derivative of the critical current decoherence between the 
+    states given  in `states` with respect to `grad_el`.
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object.
+        states:
+            A tuple `(m, n)` of states to consider charge decoherence between.
+        grad_el:
+            The circuit element to differentiate with respect to
+    """
     dec_rate_grad = 0
     for EJ_el, B_idx in cr._memory_ops['cos']:
         partial_omega_mn = sqf.numpy(cr._get_partial_omega_mn(
@@ -305,7 +323,7 @@ def partial_cc_dec(
             grad_el,
             states
         )
-        A = EJ_el.A * EJ_el.get_value()
+        A = sqf.numpy(EJ_el.A * EJ_el.get_value())
         partial_A = EJ_el.A if grad_el is EJ_el else 0
 
         dec_rate_grad += partial_dephasing_rate(A,
@@ -316,7 +334,7 @@ def partial_cc_dec(
     return dec_rate_grad
 
 ###############################################################################
-# Flux Noise
+# Flux noise
 ###############################################################################
 
 def partial_squared_H_phi(
@@ -324,6 +342,18 @@ def partial_squared_H_phi(
     loop: Loop,
     grad_el: Union[Capacitor, Inductor, Junction]
 ):
+    """ Calculates the second derivative of the Hamiltonian of `cr` with 
+    respect to the external flux through `loop` and `grad_el`
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object to differentiate the Hamiltonian of
+        loop:
+            The loop with external flux to differentiate with respect to
+        grad_el:
+            The circuit element to differentiate with respect to
+    """
     if isinstance(grad_el, Capacitor):
         return 0
 
@@ -342,13 +372,26 @@ def partial_squared_H_phi(
     else:
         raise NotImplementedError
 
-
 def partial_squared_omega_mn_phi(
     cr: Circuit,
     loop: Loop,
     grad_el: Union[Capacitor, Inductor, Junction],
     states: Tuple[int, int]
 ):
+    """ Calculate the second derivative of the difference between the `m`th 
+    and `n`th eigenfrequencies with respect to `loop` and `grad_el`.
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object to differentiate the eigenfrequencies of
+        loop:
+            A loop in `cr` to differentiate with respect to
+        grad_el:
+            A circuit element to differentiate with respect to
+        states:
+            The numbers `(m, n)` of the eigenfrequencies to differentiate.
+    """
     partial_H = cr._get_partial_H(loop)
     partial_H_squared = partial_squared_H_phi(cr, loop, grad_el)
 
@@ -361,7 +404,17 @@ def partial_flux_dec(
     grad_el: Element,
     states: Tuple[int, int]
 ):
-    """The A is independent of all elements
+    """ Calculate the derivative of the flux decoherence between the states 
+    given in `states` with respect to `grad_el`.
+
+    Parameters
+    ----------
+        cr:
+            The `Circuit` object.
+        states:
+            A tuple `(m, n)` of states to consider charge decoherence between.
+        grad_el:
+            The circuit element to differentiate with respect to
     """
     dec_rate_grad = 0
     for loop in cr.loops:
@@ -384,22 +437,22 @@ def partial_flux_dec(
 
 
 ###############################################################################
-# Torch Nodes
+# Torch nodes
 ###############################################################################
 
 def dec_rate_cc_torch(circuit: Circuit, states: Tuple[int, int]):
-    return DecRateCC.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]), 
-                           circuit, 
+    return DecRateCC.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]),
+                           circuit,
                            states)
 
 def dec_rate_charge_torch(circuit: Circuit, states: Tuple[int, int]):
-    return DecRateCharge.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]), 
-                               circuit, 
+    return DecRateCharge.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]),
+                               circuit,
                                states)
 
 def dec_rate_flux_torch(circuit: Circuit, states: Tuple[int, int]):
-    return DecRateFlux.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]), 
-                             circuit, 
+    return DecRateFlux.apply(torch.stack(circuit.parameters) if circuit.parameters else torch.tensor([]),
+                             circuit,
                              states)
 
 class DecRateCC(Function):
@@ -409,13 +462,26 @@ class DecRateCC(Function):
         circuit: 'Circuit',
         states: Tuple[int, int]
     ) -> Tensor:
-        pass
+        return torch.as_tensor(circuit._dec_rate_cc_np(states))
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        circuit_parameters, circuit, states = inputs
+
+        ctx.circuit = circuit.safecopy(save_eigs=True)
+        ctx.states  = states
 
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output) -> Tuple[Tensor]:
-        pass
+        output_grad = torch.zeros(len(ctx.circuit._parameters))
 
+        for idx, elem in enumerate(ctx.circuit._parameters.keys()):
+            output_grad[idx] = grad_output * partial_cc_dec(ctx.circuit,
+                                                            elem,
+                                                            ctx.states)
+
+        return output_grad, None, None
 
 class DecRateCharge(Function):
     @staticmethod
@@ -424,12 +490,26 @@ class DecRateCharge(Function):
         circuit: 'Circuit',
         states: Tuple[int, int]
     ) -> Tensor:
-        pass
+        return torch.as_tensor(circuit._dec_rate_charge_np(states))
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        circuit_parameters, circuit, states = inputs
+
+        ctx.circuit = circuit.safecopy(save_eigs=True)
+        ctx.states  = states
 
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output) -> Tuple[Tensor]:
-        pass
+        output_grad = torch.zeros(len(ctx.circuit._parameters))
+
+        for idx, elem in enumerate(ctx.circuit._parameters.keys()):
+            output_grad[idx] = grad_output * partial_charge_dec(ctx.circuit,
+                                                                elem,
+                                                                ctx.states)
+
+        return output_grad, None, None
 
 class DecRateFlux(Function):
     @staticmethod
@@ -439,7 +519,7 @@ class DecRateFlux(Function):
         states: Tuple[int, int]
     ) -> Tensor:
         return torch.as_tensor(circuit._dec_rate_flux_np(states))
-    
+
     @staticmethod
     def setup_context(ctx, inputs, output):
         circuit_parameters, circuit, states = inputs
@@ -456,5 +536,5 @@ class DecRateFlux(Function):
             output_grad[idx] = grad_output * partial_flux_dec(ctx.circuit,
                                                               elem,
                                                               ctx.states)
-        
+
         return output_grad, None, None

qubit_discovery/tests/test_T2_grad.py
@@ -1,15 +1,18 @@
 import numpy as np
 
 import SQcircuit as sq
-from SQcircuit import  Circuit
+from SQcircuit import Capacitor, Circuit, Inductor, Junction
 from SQcircuit import functions as sqf
 from SQcircuit import units as unt
 from qubit_discovery.losses.T2_functions import (
     partial_cc_dec,
     partial_charge_dec,
     partial_flux_dec,
+    dec_rate_cc_torch,
+    dec_rate_charge_torch,
     dec_rate_flux_torch,
 )
+from qubit_discovery.optimization.utils import set_grad_zero
 
 ###############################################################################
 # Circuit helper functions
@@ -49,7 +52,13 @@ def build_fl_elements(EC, EL, EJ, flux, flux_dist='junctions'):
 # Gradient check helper functions
 ###############################################################################
 
-def test_grad_func(cr: Circuit, 
+UNIT_FACTORS = {
+    Capacitor: lambda EC: sqf.numpy(-unt.e**2/2/sq.Capacitor(EC, 'GHz')._value**2/(2*np.pi*unt.hbar) / 1e9),
+    Inductor: lambda EL: sqf.numpy(-(unt.Phi0/2/np.pi)**2/sq.Inductor(EL, 'GHz')._value**2/(2*np.pi*unt.hbar)/1e9),
+    Junction: 1 / 2 / np.pi / 1e9
+}
+
+def do_grad_func_test(cr: Circuit, 
                    cr_delta: Circuit,
                    delta,
                    unit_factor,
@@ -70,36 +79,11 @@ def test_grad_func(cr: Circuit,
     print('Numeric:', grad_numeric, 'Analytic:', grad_analytic)
     assert np.isclose(grad_numeric, grad_analytic, rtol=1e-2)
 
-###############################################################################
-# Dec rate derivative tests 
-###############################################################################
-
-def test_cc_grad() -> None:
-    EC, EL, EJ = 3.6, 0.46, 50
-    delta = 1e-8
-    cr = build_fl_elements(EC, EL, EJ, 0.5, 'junctions')
-    cr_delta = build_fl_elements(EC, EL, EJ + delta, 0.5, 'junctions')
-
-    circuits = [cr, cr_delta]
-    for circuit in circuits:
-        circuit.set_trunc_nums([300])
-        _ = circuit.diag(150)
-
-    unit_factor = 1 / 2 / np.pi / 1e9
-    grad_numeric = (
-        cr_delta.dec_rate('cc', states=(0, 1))
-        - cr.dec_rate('cc', states=(0, 1))
-    ) / delta * unit_factor
-
-    grad_analytic = partial_cc_dec(cr, cr.elements[(0, 1)][1], (0, 1))
-
-    print('Numeric:', grad_numeric, 'Analytic:', grad_analytic)
-    assert np.isclose(grad_numeric, grad_analytic, rtol=1e-2)
-
-
-def test_flux_grad_EJ() -> None:
-    EC, EL, EJ = 3.6, 0.46, 20.5
-    delta = 1e-6
+def do_test_dec_rate_fluxonium(
+        dec_rate_func, dec_grad,
+        EC = 3.6, EL = 0.46, EJ = 20.5,
+        delta = 1e-6
+    ):
     flux_points = [1e-2, 0.25, 0.5 - 1e-2, 0.5 + 1e-2, 0.75]
 
     for optim_mode in [True, False]:
@@ -107,87 +91,182 @@ def test_flux_grad_EJ() -> None:
         for flux_point in flux_points:
             print('Optim mode=', optim_mode, 'Flux =', flux_point)
             cr = build_fl_elements(EC, EL, EJ, flux_point, 'junctions')
-            cr_delta = build_fl_elements(EC, EL, EJ + delta, flux_point, 'junctions')
+            cr_delta_J  = build_fl_elements(EC, EL, EJ + delta, flux_point, 'junctions')
+            cr_delta_L = build_fl_elements(EC, EL + delta, EJ, flux_point, 'junctions')
+            cr_delta_C = build_fl_elements(EC + delta, EL, EJ, flux_point, 'junctions')
 
-            unit_factor = 1 / 2 / np.pi / 1e9
-            test_grad_func(
-                cr, cr_delta, delta, unit_factor,
-                lambda x: x.dec_rate('flux', states=(0, 1)),
-                lambda x: partial_flux_dec(x, x.elements[(0, 1)][1], (0, 1))
+            do_grad_func_test(
+                cr, cr_delta_J, delta, UNIT_FACTORS[Junction],
+                lambda x: dec_rate_func(x, states=(0, 1)),
+                lambda x: dec_grad(x, x.elements[(0, 1)][1], (0,1))
+            )
+            do_grad_func_test(
+                cr, cr_delta_L, delta, UNIT_FACTORS[Inductor](EL),
+                lambda x: dec_rate_func(x, states=(0, 1)),
+                lambda x: dec_grad(x, x.elements[(0, 1)][0], (0,1))
+            )
+            do_grad_func_test(
+                cr, cr_delta_C, delta, UNIT_FACTORS[Capacitor](EC),
+                lambda x: dec_rate_func(x, states=(0, 1)),
+                lambda x: dec_grad(x, x.elements[(0, 1)][2], (0,1))
             )
             print('\n')
         print('~'*10)
 
-def test_flux_grad_EL() -> None:
-    EC, EL, EJ = 3.6, 0.46, 10.2
-    delta = 1e-6
-    flux_points = [1e-2, 0.25, 0.5 - 1e-2, 0.5 + 1e-2, 0.75]
-
-    for optim_mode in [False, True]:
-        sq.set_optim_mode(optim_mode)
-        for flux_point in flux_points:
-            print('Optim mode=', optim_mode, 'Flux =', flux_point)
-            cr = build_fl_elements(EC, EL, EJ, flux_point, 'junctions')
-            cr_delta = build_fl_elements(EC, EL+delta, EJ, flux_point, 'junctions')
-
-            unit_factor = sqf.numpy(-(unt.Phi0/2/np.pi)**2/cr.elements[(0, 1)][0]._value**2/(2*np.pi*unt.hbar)/1e9)
-            test_grad_func(
-                cr, cr_delta, delta, unit_factor,
-                lambda x: x.dec_rate('flux', states=(0, 1)),
-                lambda x: partial_flux_dec(x, x.elements[(0, 1)][0], (0, 1))
-            )
-
-def test_charge_grad() -> None:
-    EC, EJ = 3.6, 10
-    delta = 1e-4
-    flux_point = 0.5
-    charge_points= [0.1] #[1e-2, 0.2, 0.4, 0.5 - 1e-2, 0.5 + 1e-2, 0.6, 0.8, 1-1e-2]
-
-    for charge_offset in charge_points:
-        cr = build_trans_elements(EC, EJ, flux_point, 'junctions')
-        cr_delta = build_trans_elements(EC + delta, EJ, flux_point, 'junctions')
-
-        for circuit in [cr, cr_delta]:
-            circuit.set_trunc_nums([200])
-            # Needs to be offset from 0.5, I think
-            circuit.set_charge_offset(1, charge_offset)
-            _ = circuit.diag(100)
-
-        unit_factor = sqf.numpy(unt.e**2/2/sq.Capacitor(EC, 'GHz')._value**2/(2*np.pi*unt.hbar) / 1e9)
-        test_grad_func(
-            cr, cr_delta, delta, unit_factor,
-            lambda x: x.dec_rate('charge', states=(0, 1)),
-            lambda x: partial_charge_dec(x, x.elements[(0, 1)][1], (0, 1))
-        )
-
-###############################################################################
-# Torch test functions
-###############################################################################
-
-
-def test_torch_flux_grad() -> None:
-    EC, EL, EJ = 3.6, 0.46, 20.5
-    delta = 1e-6
-    flux_point = 0.5 - 1e-2
-
-    sq.set_optim_mode(True)
-    cr = build_fl_elements(EC, EL, EJ, flux_point, 'junctions')
-    cr_delta = build_fl_elements(EC, EL, EJ + delta, flux_point, 'junctions')
-
+def do_test_torch_func(cr: Circuit, 
+                    cr_delta: Circuit,
+                    delta,
+                    unit_factor,
+                    np_func,
+                    torch_func,
+                    elem) -> None:
+    """
+    Check the gradient of `np_func` numerically at two points `cr` and
+    `cr_delta` against the outuput of `func_grad` at `cr`.
+    """
     circuits = [cr, cr_delta]
     for circuit in circuits:
         circuit.set_trunc_nums([300])
         _ = circuit.diag(100)
 
-    unit_factor = 1 / 2 / np.pi / 1e9
-    grad_numeric = sqf.numpy(
-        cr_delta.dec_rate('flux', states=(0, 1))
-        - cr.dec_rate('flux', states=(0, 1))
-    ) / delta * unit_factor
+    grad_numeric = sqf.numpy(np_func(cr_delta) - np_func(cr)) / delta * unit_factor
 
-    flux_dec = dec_rate_flux_torch(cr, (0, 1))
-    flux_dec.backward()
-    grad_torch = cr.elements[(0, 1)][1]._value.grad
+    dec_rate = torch_func(cr)
+    dec_rate.backward()
+    grad_torch = elem._value.grad
 
-    print('Flux =', flux_point, 'Numeric:', grad_numeric, 'Torch:', grad_torch)
+    print('Numeric:', grad_numeric, 'Torch:', grad_torch)
     assert np.isclose(grad_numeric, grad_torch, rtol=1e-2)
+
+def do_test_dec_torch_fluxonium(
+        dec_rate_np, dec_rate_torch,
+        EC = 3.6, EL = 0.46, EJ = 20.5,
+        delta = 1e-6
+    ):
+    flux_points = [1e-2, 0.25, 0.5 - 1e-2, 0.5 + 1e-2, 0.75]
+
+    sq.set_optim_mode(True)
+    for flux_point in flux_points:
+        print('phi_ext =', flux_point)
+        cr = build_fl_elements(EC, EL, EJ, flux_point, 'junctions')
+        cr_delta_J  = build_fl_elements(EC, EL, EJ + delta, flux_point, 'junctions')
+        cr_delta_L = build_fl_elements(EC, EL+delta, EJ, flux_point, 'junctions')
+        cr_delta_C = build_fl_elements(EC + delta, EL, EJ, flux_point, 'junctions')
+
+        do_test_torch_func(
+            cr, cr_delta_J, delta, UNIT_FACTORS[Junction],
+            lambda x: dec_rate_np(x, states=(0, 1)),
+            lambda x: dec_rate_torch(x, (0,1)),
+            cr.elements[(0, 1)][1]
+        )
+        set_grad_zero(cr) # don't forget this!
+        do_test_torch_func(
+            cr, cr_delta_L, delta, UNIT_FACTORS[Inductor](EL),
+            lambda x: dec_rate_np(x, states=(0, 1)),
+            lambda x: dec_rate_torch(x, (0,1)),
+            cr.elements[(0, 1)][0]
+        )
+        set_grad_zero(cr)
+        do_test_torch_func(
+            cr, cr_delta_C, delta, UNIT_FACTORS[Capacitor](EC),
+            lambda x: dec_rate_np(x, states=(0, 1)),
+            lambda x: dec_rate_torch(x, (0,1)),
+            cr.elements[(0, 1)][2]
+        )
+        print('\n')
+
+def do_test_dec_torch_trans(
+        dec_rate_np, dec_rate_torch,
+        EC = 3.6, EJ = 10.2,
+        delta = 1e-6
+    ):
+    flux_point = 0.5
+    charge_points= [1e-2, 0.2, 0.4, 0.5 - 1e-2, 0.5 + 1e-2, 0.6, 0.8, 1-1e-2]
+
+    sq.set_optim_mode(True)
+    for charge_offset in charge_points:
+        print('ng =', charge_offset)
+        cr = build_trans_elements(EC, EJ, flux_point, 'junctions')
+        cr_delta_C = build_trans_elements(EC + delta, EJ, flux_point, 'junctions')
+        cr_delta_J = build_trans_elements(EC, EJ + delta, flux_point, 'junctions')
+
+        for circ in [cr, cr_delta_C, cr_delta_J]:
+            circ.set_charge_offset(1, charge_offset)
+
+        do_test_torch_func(
+            cr, cr_delta_J, delta, UNIT_FACTORS[Junction],
+            lambda x: dec_rate_np(x, states=(0, 1)),
+            lambda x: dec_rate_torch(x, (0,1)),
+            cr.elements[(0, 1)][0]
+        )
+        set_grad_zero(cr) # don't forget this!
+        do_test_torch_func(
+            cr, cr_delta_C, delta, UNIT_FACTORS[Capacitor](EC),
+            lambda x: dec_rate_np(x, states=(0, 1)),
+            lambda x: dec_rate_torch(x, (0,1)),
+            cr.elements[(0, 1)][1]
+        )
+        print('\n')
+
+###############################################################################
+# Dec rate derivative tests 
+###############################################################################
+
+def test_cc_grad() -> None:
+    do_test_dec_rate_fluxonium(
+        lambda cr, states: cr.dec_rate('cc', states=states),
+        lambda cr, elem, states: partial_cc_dec(cr, elem, states)
+    )
+
+def test_charge_grad() -> None:
+    EC, EJ = 3.6, 20
+    delta = 1e-4
+    flux_point = 0.5
+    charge_points= [1e-2, 0.2, 0.4, 0.5 - 1e-2, 0.5 + 1e-2, 0.6, 0.8, 1-1e-2]
+
+    for charge_offset in charge_points:
+        cr = build_trans_elements(EC, EJ, flux_point, 'junctions')
+        cr_delta_C = build_trans_elements(EC + delta, EJ, flux_point, 'junctions')
+        cr_delta_J = build_trans_elements(EC, EJ + delta, flux_point, 'junctions')
+
+        for circ in [cr, cr_delta_C, cr_delta_J]:
+            circ.set_charge_offset(1, charge_offset)
+
+        do_grad_func_test(
+            cr, cr_delta_C, delta, UNIT_FACTORS[Capacitor](EC),
+            lambda x: x.dec_rate('charge', states=(0, 1)),
+            lambda x: partial_charge_dec(x, x.elements[(0, 1)][1], (0, 1))
+        )
+        do_grad_func_test(
+            cr, cr_delta_J, delta, UNIT_FACTORS[Junction],
+            lambda x: x.dec_rate('charge', states=(0, 1)),
+            lambda x: partial_charge_dec(x, x.elements[(0, 1)][0], (0, 1))
+        )
+
+def test_flux_grad() -> None:
+    do_test_dec_rate_fluxonium(
+        lambda cr, states: cr.dec_rate('flux', states=states),
+        lambda cr, elem, states: partial_flux_dec(cr, elem, states)
+    )
+
+###############################################################################
+# Torch test functions
+###############################################################################
+
+def test_torch_flux_grad() -> None:
+    do_test_dec_torch_fluxonium(
+        lambda cr, states: cr.dec_rate('flux', states=states),
+        lambda cr, states: dec_rate_flux_torch(cr, states)
+    )
+
+def test_torch_cc_grad() -> None:
+    do_test_dec_torch_fluxonium(
+        lambda cr, states: cr.dec_rate('cc', states=states),
+        lambda cr, states: dec_rate_cc_torch(cr, states)
+    )
+
+def test_torch_charge_grad() -> None:
+    do_test_dec_torch_trans(
+        lambda cr, states: cr.dec_rate('charge', states=states),
+        lambda cr, states: dec_rate_charge_torch(cr, states)
+    )