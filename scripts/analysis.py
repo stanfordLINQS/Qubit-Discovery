6117a5802106428afeee982039b162b56fb67187
taha1373 <tahar@stanford.edu>
2024-07-04 02:43:57 -0700

    remove unnecessary plotting and reducing the resolution for speed up

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,27 +1,26 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 import SQcircuit as sq
 import SQcircuit.functions as sqf
 
 
-#############################
-# Initialize circuit        #
-#############################
+################################################################################
+# Initialize circuit
+################################################################################
 
 def reset_loop_flux(circuit, flux):
     for loop in circuit.loops:
         loop.set_flux(flux)
 
 
-def reset_charge_modes(circuit, default_charge = 0):
+def reset_charge_modes(circuit, default_charge=0):
     for charge_island_idx in circuit.charge_islands.keys():
         charge_mode = charge_island_idx + 1
         circuit.set_charge_offset(charge_mode, default_charge)
 
-#############################
-# Print element values      #
-#############################
+################################################################################
+# Print element values
+################################################################################
 
 
 def round_n_sigfigs(value):
@@ -61,9 +60,9 @@ def build_circuit_topology_string(circuit):
     return output
 
 
-#############################
-# Plot charge spectrum      #
-#############################
+################################################################################
+# Plot charge spectrum
+################################################################################
 def sweep_charge_spectrum(
     circuit,
     sweep_charge_node_bools,
@@ -82,7 +81,9 @@ def sweep_charge_spectrum(
     if sum(circuit.omega == 0) == 0:
         return None
     else:
+        print("Calculating charge spectrum...")
         for charge_sweep_idx, n_g in enumerate(n_g_vals):
+            print(f"{charge_sweep_idx+1}/{n_charge_points}")
             for charge_island_idx in circuit.charge_islands.keys():
                 if sweep_charge_node_bools[charge_island_idx]:
                     circuit.set_charge_offset(
@@ -97,79 +98,39 @@ def sweep_charge_spectrum(
             x, _ = circuit.diag(n_eig)
             spectrum[:, charge_sweep_idx] = sqf.numpy(x)
 
-    spectrum -= spectrum[0, :] # Set zeroth eigenenergy to zero
+    # Set zeroth eigenfrequency to zero
+    spectrum -= spectrum[0, :]
     return n_g_vals, spectrum
 
 
-def grid_charge_spectrum(
-    circuit,
-    sweep_charge_node_bools,
-    n_eig=4,
-    charge_min=0,
-    charge_max=1,
-    default_charge=0,
-    n_charge_points=20
-):
-    if not np.count_nonzero(sweep_charge_node_bools):
-        return None
-
-    circuit_initial_flux = circuit.loops[0].value() / (2 * np.pi)
-
-    phis = [np.linspace(charge_min, charge_max, n_charge_points)] * np.count_nonzero(sweep_charge_node_bools)
-    phi_meshes = np.meshgrid(*phis) # this gets quite large if many modes
-
-    out = np.zeros_like(phi_meshes[0])
-
-    it = np.nditer(phi_meshes[0], flags=['multi_index'])
-    reset_loop_flux(circuit, circuit_initial_flux)
-
-    sweep_modes = [i for i, val in enumerate(sweep_charge_node_bools) if val]
-
-    for _ in it:
-        reset_charge_modes(circuit, default_charge)
-        for idx, mode_num in enumerate(sweep_modes):
-            circuit.set_charge_offset(mode_num + 1, phi_meshes[idx][it.multi_index])
-        efreqs , _ = circuit.diag(n_eig)
-        efreqs = sqf.numpy(efreqs)
-        out[it.multi_index] = efreqs[1] - efreqs[0]
-    
-    return phi_meshes, out
-
-
-def plot_1D_charge_spectrum(n_g_vals, spectrum, ax, n_eig=4):
+def plot_1d_charge_spectrum(n_g_vals, spectrum, ax, n_eig=4):
     for eigen_idx in range(n_eig):
         ax.plot(n_g_vals, spectrum[eigen_idx, :])
 
     ax.set_xlabel(r"$n_g$")
     ax.set_ylabel(r"$f_i-f_0$ [GHz]")
 
+################################################################################
+# Plot flux spectrum
+################################################################################
 
-def plot_2D_charge_spectrum(ng1, ng2, frequency, ax):
-    c = ax.pcolormesh(ng1, ng2, frequency)
-    plt.colorbar(c)
 
-
-#############################
-# Plot flux spectrum        #
-#############################
 def calculate_flux_spectrum(
     circuit,
     flux_min=0,
-    flux_max=1,
+    flux_max=0.5,
     n_eig=4,
-    count=200,
-    center_count=10,
-    delta=0.03
+    count=30,
 ):
     reset_charge_modes(circuit)
 
     # Currently assume one loop
     initial_flux_value = circuit.loops[0].value() / (2 * np.pi)
     flux_values = np.linspace(flux_min, flux_max, count)
-    center_values = np.linspace(0.5 - delta, 0.5 + delta, center_count)
-    flux_values = np.sort(np.concatenate((flux_values, center_values)))
-    flux_spectrum = np.zeros((n_eig, count + center_count))
+    flux_spectrum = np.zeros((n_eig, count))
+    print("Calculating flux spectrum...")
     for flux_idx, flux in enumerate(flux_values):
+        print(f"{flux_idx+1}/{count}")
         for loop in circuit.loops:
             loop.set_flux(flux)
         circuit.update()
@@ -193,42 +154,3 @@ def plot_flux_spectrum(flux_vals, spectrum, ax, n_eig=-1):
 
     ax.set_xlabel(r"$\Phi_{ext}/\Phi_0$")
     ax.set_ylabel(r"$f_i-f_0$ [GHz]")
-
-
-#############################
-# Plot eigenfunctions       #
-#############################
-
-
-def calc_state(
-    circuit,
-    n,
-    num_modes,
-    num_points=400
-):
-    phi_range = []
-    for i in range(num_modes):
-        phi_range.append(np.linspace(-1, 1, num_points))
-
-    return phi_range, circuit.eig_phase_coord(n, grid=phi_range)
-
-
-def plot_state_phase(phi_range, 
-                     state, 
-                     ax,
-                     plot_type='abs'):
-    ax.set_xlabel(r'$\phi_1$')
-    ax.set_ylabel(r'$\phi_2$')
-
-    if plot_type == 'abs':
-        ax.pcolor(phi_range[0], phi_range[1], np.abs(state)**2,
-                   cmap="binary", shading='auto')
-        ax.set_title(r'State Magnitude $|\psi(\phi_1,\phi_2)|^2$')
-    if plot_type == 'real':
-        ax.pcolor(phi_range[0], phi_range[1], np.real(state),
-                   cmap="binary", shading='auto')
-        ax.set_title(r'Re$[\psi(\phi_1,\phi_2)]$')
-    if plot_type == 'imag':
-        ax.pcolor(phi_range[0], phi_range[1], np.imag(state),
-                   cmap="binary", shading='auto')
-        ax.set_title(r'Im$[\psi(\phi_1,\phi_2)]$')
